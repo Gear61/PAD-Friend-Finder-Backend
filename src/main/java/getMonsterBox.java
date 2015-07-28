a0903a2a258dfef62bc32c974dfc504982f06b7b
Gear61 <chessnone@yahoo.com>
2015-07-28 16:47:56 -0700

    Minor cleanup and refactoring

--- src/main/java/getMonsterBox.java
+++ src/main/java/getMonsterBox.java
@@ -1,26 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.*;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class getBox {
-    public static void get_Box(Connection connection, HttpServletResponse resp, String id) throws JSONException, IOException {
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class getMonsterBox {
+    public static void getMonsterBox(Connection connection, HttpServletResponse resp, String id) throws JSONException, IOException {
         try {
             String get_Box_SQL = "Select * from monster where pad_ID = ? ";
             PreparedStatement stmt = connection.prepareStatement(get_Box_SQL);
@@ -40,7 +30,8 @@ public class getBox {
             resp.getWriter().print(monster_array.toString());
         }
         catch (SQLException e) {
-            resp.getWriter().print("get_Box error: " + Main.getStackTrace(e));
+            resp.setStatus(500);
+            resp.getWriter().print("getMonsterBox error: " + Main.getStackTrace(e));
         }
     }
 }
