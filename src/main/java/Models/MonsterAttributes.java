3c80006f28597b92c67a6167c89c422619f13293
Gear61 <chessnone@yahoo.com>
2015-08-21 22:56:15 -0700

    Adding basis for monster serving

---
+++ src/main/java/Models/MonsterAttributes.java
@@ -0,0 +1,49 @@
+package Models;
+
+/**
+ * Created by alexanderchiou on 8/21/15.
+ */
+public class MonsterAttributes
+{
+    private String name;
+    private int level;
+    private int skillLevel;
+    private int awakenings;
+    private int monsterId;
+
+    // For hard-coded hashmap
+    public MonsterAttributes(String name, int level, int skillLevel, int awakenings, int monsterId)
+    {
+        this.name = name;
+        this.level = level;
+        this.skillLevel = skillLevel;
+        this.awakenings = awakenings;
+        this.monsterId = monsterId;
+    }
+
+    // Getters
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public int getSkillLevel()
+    {
+        return skillLevel;
+    }
+
+    public int getAwakenings()
+    {
+        return awakenings;
+    }
+
+    public String getImageUrl()
+    {
+        return "http://puzzledragonx.com/en/img/book/" + String.valueOf(monsterId) + ".png";
+    }
+}
