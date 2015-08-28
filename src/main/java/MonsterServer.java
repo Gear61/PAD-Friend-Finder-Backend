0d182e709e2af6571bb80443fa49c9fea809f7b6
Jonathan Chiou <jchio001@ucr.edu>
2015-08-27 22:48:24 -0700

    Stupid typhon already added.

--- src/main/java/MonsterServer.java
+++ src/main/java/MonsterServer.java
@@ -158,7 +158,6 @@ public class MonsterServer
         monsters.add(new MonsterAttributes("Awoken Leilan", 99, 8, 5, 2073));
         monsters.add(new MonsterAttributes("Awoken Haku", 99, 8, 5, 2076));
         monsters.add(new MonsterAttributes("Green Gleaming Star Kirin, Sakuya", 99, 6, 9, 2077));
-        monsters.add(new MonsterAttributes("Lightning Black Dragonbound, Typhon", 99, 6, 6, 1949));
     }
 
     public List<MonsterAttributes> getMonsters()
